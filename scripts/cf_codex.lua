ee0ad3568e0169550daf1560bd4833befc0c4ee2
JamesTheMaker <jamesthemaker2005@gmail.com>
2024-09-16 16:44:00 -0400

    cf_codex.lua doOnce

--- scripts/cf_codex.lua
+++ scripts/cf_codex.lua
@@ -1,15 +1,18 @@
 function update()
-    if not fireableItem.coolingDown() and fireableItem.firing() then
+    if self.doOnce then
+        if fireableItem.coolingDown() or not fireableItem.firing() then 
+            self.doOnce = false 
+        end
+    elseif fireableItem.coolingDown() and fireableItem.firing() then
         for _, playerId in pairs(world.playerQuery(fireableItem.ownerAimPosition(), 256)) do
             if world.entityHandItem(playerId, "primary") == item.name() or world.entityHandItem(playerId, "alt") == item.name() then
-                local dir = root.itemConfig(item.name()).directory
-
-                local length = string.len(item.name()) - 6
-                local codex = string.sub(item.name(), 1, length)
-
-                local file = dir .. codex .. ".codex"
+                local file = root.itemConfig(item.name()).directory -- directory
+                    .. string.sub(item.name(), 1, string.len(item.name()) - 6) -- name
+                    .. ".codex"
 
                 world.sendEntityMessage(playerId, "cf_learncodex", file)
+
+                self.doOnce = true
             end
         end
     end
