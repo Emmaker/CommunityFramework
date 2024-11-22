a42a532d5598282e1f2b6275b960fd63b6caaf3f
Emmaker <emmaker0000@gmail.com>
2024-11-21 22:08:14 -0500

    unsafe functions (not yet tested)

--- scripts/cfpower.lua
+++ scripts/cfpower.lua
@@ -32,8 +32,14 @@ end
 
 -- void cfpower.setMaxPower(int power)
 function cfpower.setMaxPower(power)
+  if type(power) == "number" then
+    cfpower._setMaxPower(power)
+    cfpower.setPower(storage.power)
+  end
+end
+
+function cfpower._setMaxPower(power)
   storage.maxPower = power
-  cfpower.setPower(storage.power)
 end
 
 -- int cfpower.getPower()
@@ -43,29 +49,50 @@ end
 
 -- int cfpower.setPower(int power)
 function cfpower.setPower(power)
-  pPower = storage.power
-  storage.power = util.clamp(power, 0, storage.maxPower)
+  if type(power) == "number" then
+    pPower = storage.power
+    cfpower._setPower(util.clamp(power, 0, storage.maxPower))
 
-  return storage.power - pPower
+    return storage.power - pPower
+  end
+end
+
+function cfpower._setPower(power)
+  storage.power = power
 end
 
 -- int cfpower.createPower(int power)
 function cfpower.createPower(power)
-  return cfpower.setPower(storage.power + power)
+  if type(power) == "number" then
+    return cfpower._setPower(util.clamp(storage.power + power, 0, storage.maxPower))
+  end
+end
+
+function cfpower._createPower(power)
+  return cfpower._setPower(storage.power + power)
 end
 
 -- bool cfpower.consumePower(int power)
 function cfpower.consumePower(power)
-  if storage.power >= power then
-    cfpower.setPower(storage.power - power)
-    return storage.power
-  end
+  if type(power) == "number" then
+    if storage.power >= power then
+      cfpower._consumePower(power)
+      return storage.power
+    end
 
-  return storage.power - power
+    return storage.power - power
+  end
+end
+
+function cfpower._consumePower(power)
+  cfpower.setPower(storage.power - power)
 end
 
 -- int, int cfpower.pushPower(int nodeID, int power, [bool alternating], [int voltage])
 function cfpower.pushPower(nodeID, power, alternating, voltage)
+  voltage = voltage or storage.voltage
+  alternating = alternating or false
+
   local outputTable = object.getOutputNodeIds(nodeID)
   local outputTableSize = util.tableSize(outputTable)
   if outputTableSize < 1 or storage.power < power then
@@ -76,32 +103,43 @@ function cfpower.pushPower(nodeID, power, alternating, voltage)
   local successes = 0
 
   for i, _ in pairs(outputTable) do
-    local message = {
-      power = power,
-      voltage = voltage or storage.voltage,
-      alternating = alternating or false
-    }
+    result = cfpower._pushPower(id)
 
-    cfpower.consumePower(power)
-    promise = world.sendEntityMessage(i, "cfpower", message)
-
-    while not promise do end
-    if promise:result() and type(promise:result()) == "table" then
-      message = promise:result()
-      if message.voltage and message.voltage > storage.voltage then
-        storage.power = 0
-        object.setOutputNodeLevel(nodeID, false)
-        return -1
-      end
-
+    if result == nil then
+      cfpower.createPower(power)
+      object.setOutputNodeLevel(nodeID, false)
+    elseif result < 0 then
+      storage.power = 0
+      object.setOutputNodeLevel(nodeID, false)
+      return result
+    else
       cfpower.createPower(promise:result().power)
       object.setOutputNodeLevel(nodeID, true)
       successes = successes + 1
-    else
-      cfpower.createPower(power)
-      object.setOutputNodeLevel(nodeID, false)
     end
   end
 
   return successes
+end
+
+function cfpower._pushPower(id, power, alternating, voltage)
+  local message = {
+    power = power,
+    voltage = voltage,
+    alternating = alternating
+  }
+
+  promise = world.sendEntityMessage(i, "cfpower", message)
+
+  while not promise do end
+  if promise:result() and type(promise:result()) == "table" then
+    message = promise:result()
+    if message.voltage and message.voltage > storage.voltage then
+      return -1
+    end
+
+    return promise:result().power
+  else
+    return nil
+  end
 end
